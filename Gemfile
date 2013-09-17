62c1711712a72dd226b5fe0543499ce477863795
Carlos Cervantes <CCER562@yahoo.com>
2013-09-16 17:51:01 -0700

    aws

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
-ruby '1.9.3'
+#ruby '1.9.3'
+#gem 'rake', '0.9.2.2'
 #export PATH=$PATH:~/AWS/AWS-ElasticBeanstalk-CLI-2.5.1/eb/macosx/python2.7/
 
 gem 'rails', '4.0.0' # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- config/application.rb
+++ config/application.rb
@@ -17,8 +17,7 @@ module Gumball
     # config.time_zone = 'Central Time (US & Canada)'
 
     config.assets.enabled = true
-
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    #config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
